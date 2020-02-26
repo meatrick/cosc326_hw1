106eb5907c28430831082cf7b8f8b1a38ceb6357
Patrick Smith <smithpm@usc.edu>
2020-02-25 19:56:39 -0800

    first commit

---
+++ email.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main () {
+	cout << "Give input:" << endl;
+
+	string line;
+
+	while (cin >> line) {
+		
+
+
+	}
+
+	return 0;
+}
