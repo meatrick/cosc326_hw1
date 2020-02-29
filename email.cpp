294d7213ae23197c6ea1fd40517ec3ef100f9c56
Patrick Smith <smithpm@usc.edu>
2020-02-28 19:34:27 -0800

    domain extension recognition in dev, some bugs

--- email.cpp
+++ email.cpp
@@ -5,10 +5,16 @@
 #include <map>
 #include <algorithm>
 
+#define NUM_VALID_DOMAIN_NAMES 6
+
 using namespace std;
 
-enum charType {upper_letter, lower_letter, at, underscore, dot, hyphen, unrecognized};
-struct Email {string name = ""; bool at = false; bool domain = false; bool dot = false; bool extension = false;};
+enum charType {upper_letter, lower_letter, at, underscore, dot, hyphen, number, unrecognized};
+charType getCharType(char c);
+bool isValidDomainType(charType type);
+string domain_names[] = {"co.nz" , "com.au", "co.ca", "com", "co.us", "co.uk"};
+
+// struct Email {string name = ""; bool at = false; bool domain = false; bool dot = false; bool extension = false;};
 // enum state {empty, on_name, read_underscore, }
 
  
@@ -30,36 +36,15 @@ int main () {
 			break;
 		}
 		
-		Email email;
+		// Email email;
 
 		// first pass: convert _at_ and _dot_
 		for (int i = 0; i < line_len; i++) {
 			char c = line[i];
-			charType type;
+			charType type = getCharType(c);
 
-
-			// determine type of character
-			if (c >= 65 && c <= 90) {
-				type = upper_letter;
-			}
-			else if (c >= 97 && c <= 122) {
-				type = lower_letter;
-			}
-			else if (c == 64) {
-				type = at;
-			}
-			else if (c == 95) {
-				type = underscore;
-			}
-			else if (c == 46) {
-				type = dot;
-			}
-			else if (c == 45) {
-				type = hyphen;
-			}
-			else {
-				type = unrecognized;
-				// throw error: unrecognized character
+			if (type == unrecognized) {
+				errorMessage = "unrecognized character(s)";
 			}
 
 			
@@ -107,6 +92,50 @@ int main () {
 				errorMessage = "missing @";
 			}
 
+
+			// check that there are valid domain and mailbox names
+			if (num_ats == 1) {
+				size_t at_pos = line.find("@");
+				charType type1 = getCharType(line[at_pos + 1]);
+				
+				if (!isValidDomainType(type1)) {
+					errorMessage = "Invalid domain name";
+				}
+
+				charType type2 = getCharType(line[0]);
+				if (!isValidDomainType(type2)) {
+					errorMessage = "Invalid mailbox name";
+				}
+			}
+
+			
+			// TODO: check for valid domain extension
+			bool valid_ext = false;
+			for (int i = 0; i < NUM_VALID_DOMAIN_NAMES; i++) {
+				string domain_ext = domain_names[i];
+				size_t ext_size = domain_ext.size();
+				size_t extension_pos = line.find(domain_ext);
+				size_t last_pos = line.size() - 1;
+				if (extension_pos != string::npos) {
+					valid_ext = true;
+					if (extension_pos + ext_size != last_pos + 1) {
+						valid_ext = false; // not in the last position
+					}
+					else break;
+				}
+			}
+			if (!valid_ext) {
+				errorMessage = "no valid domain extension";
+			}
+
+
+
+			// convert uppercase to lowercase
+			if (type == upper_letter) {
+				line[i] += 32;
+			}
+
+
 			// error handling
 			if (errorMessage != "") {
 				line = line_in + " <-- " + errorMessage;
@@ -128,4 +157,48 @@ int main () {
 	}
 
 	return 0;
+}
+
+charType getCharType(char c) {
+	charType type;
+
+	cout << c << endl;
+
+	// determine type of character
+	if (c >= 65 && c <= 90) {
+		type = upper_letter;
+	}
+	else if (c >= 97 && c <= 122) {
+		type = lower_letter;
+	}
+	else if (c == 64) {
+		type = at;
+	}
+	else if (c == 95) {
+		type = underscore;
+	}
+	else if (c == 46) {
+		type = dot;
+	}
+	else if (c == 45) {
+		type = hyphen;
+	}
+	else if (c >= 48 && c <= 57) {
+		type = number;
+	}
+	else {
+		type = unrecognized;
+	}
+
+	return type;
+}
+
+bool isValidDomainType(charType type) {
+	if (type == lower_letter || type == upper_letter
+	|| type == hyphen || type == underscore 
+	|| type == number) {
+		return true;
+	}
+
+	return false;
 }
